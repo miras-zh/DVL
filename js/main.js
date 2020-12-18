3b85650cde3af8fce6626d53576eed9dec17aaa6
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2020-12-19 03:03:28 +0600

    001

--- js/main.js
+++ js/main.js
@@ -3,11 +3,25 @@
 
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+const cllose = document.querySelector(".close");
 
-cartButton.addEventListener("click",toggleModal)
-close.addEventListener("click",toggleModal)
 
-const toggleModal = function() {
+
+cartButton.addEventListener("click",toggleModal);
+cllose.addEventListener("click",toggleModal)
+
+function toggleModal() {
   modal.classList.toggle("is-open");
 }
+// day 1 
+const authButton = document.querySelector('.button-auth');
+const authModal = document.querySelector('.modal-auth');
+
+function toggleModalAuth(){
+  authModal.classList.toggle('is-open');
+  console.log('hello')
+}
+
+console.dir(authModal)
+
+authButton.addEventListener('click',toggleModalAuth)
