44eb044f4e931c91cfca439b7ff8a868076fd4fe
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2020-12-27 08:44:38 +0600

    31min 1day

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
 
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
-const cllose = document.querySelector(".close");
+const close = document.querySelector(".close");
+let auth = false;
 
 
-
-cartButton.addEventListener("click",toggleModal);
-cllose.addEventListener("click",toggleModal)
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal)
 
 function toggleModal() {
   modal.classList.toggle("is-open");
@@ -16,12 +16,36 @@ function toggleModal() {
 // day 1 
 const authButton = document.querySelector('.button-auth');
 const authModal = document.querySelector('.modal-auth');
+const closeButtonAuth = document.querySelector('.close-auth');
+const loginForm = document.querySelector('#logInForm');
 
-function toggleModalAuth(){
+function toggleModalAuth() {
   authModal.classList.toggle('is-open');
   console.log('hello')
 }
 
 console.dir(authModal)
 
-authButton.addEventListener('click',toggleModalAuth)
+
+
+function authorized() {
+  console.log('autorized')
+}
+
+function notAuthorized() {
+  console.log('not autorized');
+
+  function logIn(){
+    console.log('login')
+  }
+
+  authButton.addEventListener('click', toggleModalAuth);
+  closeButtonAuth.addEventListener('click', toggleModalAuth);
+  loginForm.addEventListener('submit',logIn)
+}
+
+if (auth) {
+  authorized();
+} else {
+  notAuthorized();
+}
