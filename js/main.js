b968835363a909f9df28d4007d5ed846350a9856
mikos8 <65290147+mikos8@users.noreply.github.com>
2021-01-23 16:15:53 +0600

    otbety 2day 1h31min

--- js/main.js
+++ js/main.js
@@ -193,4 +193,21 @@ createCardReastaurant();
 
 //slider
 
-new Swiper = 
+new Swiper(".swiper-container", {
+  sliderPerView: 1,
+  loop: true,
+  autoplay: true,
+  grabCursor: true,
+  /*pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },*/
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
